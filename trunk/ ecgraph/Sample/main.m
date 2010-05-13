6ad80953a8ae473c8da667bad14de954c22a1e16
Tigris.Shin <Tigris.Shin@732889fa-e13d-fb8d-e484-910e4f4b8b64>
2010-05-13 01:33:40 +0000

    First Version

---
+++ trunk/ ecgraph/Sample/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ECGraphicTest
+//
+//  Created by ris on 10-4-17.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
